9995d26b10dc5e3f137a84e69d0627ec71e88d7e
mancinilucas <mancini.lucasg@gmail.com>
2023-10-26 18:10:25 -0300

    feat: adicionado cores ao projeto e aos botões selecionados

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 let total = 0;
 
-    function addGrade(grade) {
-      total += grade;
-      document.getElementById('totalGrade').textContent = total;
-    }
+function addGrade(grade) {
+  total += grade;
+  document.getElementById('totalGrade').textContent = total;
+}
 
-    function resetGrades() {
-      total = 0;
-      document.getElementById('totalGrade').textContent = total;
-    }
+function resetGrades() {
+  total = 0;
+  document.getElementById('totalGrade').textContent = total;
+  removeAllPressed();
+}
+
+
+function removeAllPressed(){
+  const removePressedButton = document.getElementById('remove-pressed')
+
+  removePressedButton.addEventListener('click', function() {
+    toggleButtons()
+  })
+}
+
+
+function toggleButtons(){
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach(button => {
+    button.classList.remove('button-pressed');
+  });
+}
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    // Usa "this" para se referir ao botão atual
+    this.classList.toggle('button-pressed');
+  })});
